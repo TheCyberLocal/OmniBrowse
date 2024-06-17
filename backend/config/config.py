b78219d0a4eb7152bf5cd74c5970910e9acbed28
Timothy Macfarlane <tim.3imt@gmail.com>
2024-06-16 20:00:10 -0500

    Updated Configuration File

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,21 +1,11 @@
-
 class Config:
     DEBUG = True
     SEARCH_ENGINES = {
-        'chrome': 'https://www.google.com/search?q=',
-        'edge': 'https://www.bing.com/search?q=',
+        'google': 'https://www.google.com/search?q=',
+        'bing': 'https://www.bing.com/search?q=',
         'brave': 'https://search.brave.com/search?q=',
-        'firefox': 'https://search.yahoo.com/search?p=',
+        'yahoo': 'https://search.yahoo.com/search?p=',
         'duckduckgo': 'https://duckduckgo.com/?q=',
         'ask': 'https://www.ask.com/web?q=',
         'aol': 'https://search.aol.com/search?q='
     }
-    SEARCH_DOMAINS = [
-        'www.google.com',
-        'www.bing.com',
-        'search.brave.com',
-        'search.yahoo.com',
-        'duckduckgo.com',
-        'www.ask.com',
-        'search.aol.com'
-    ]
